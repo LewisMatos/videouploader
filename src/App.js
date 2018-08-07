b19e01710b7f3e8c4da5932a87f07c84280a7d21
Lewis <lewm20@gmail.com>
2018-08-07 00:32:39 -0400

    add awsmobile s3Storage and Auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Storage, Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Storage.configure({ level: 'private' });
+
+class S3Image extends Component {
+  state = { src: null };
+
+  async componentDidMount() {
+    const { s3key } = this.props;
+    const src = await Storage.get(s3key);
+    this.setState({ src });
+  }
+
+  render() {
+    const { src } = this.state;
+    if (!src) return null;
+    return (
+      <article>
+        <img src={src} />
+      </article>
+    );
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+  }
+  state = {
+    files: [],
+  };
+
+  async componentDidMount() {
+    Auth.signOut();
+    const files = await Storage.list('');
+    this.setState({ files });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const file = this.fileInput.current.files[0];
+
+    Storage.put(file.name, file).then(response => {
+      debugger;
+      console.log('Storage.put', { response });
+      this.setState({ files: [this.state.files, response] });
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="NewItem" onSubmit={this.handleSubmit}>
+          <input type="file" ref={this.fileInput} />
+          <input className="full-width" type="submit" />
+        </form>
+        <section className="App-images">
+          {this.state.files.map(file => {
+            console.log(file);
+            debugger;
+            return <S3Image s3key={file.key} key={file.key} />;
+          })}
+        </section>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
+
+Amplify.configure(configuration);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
